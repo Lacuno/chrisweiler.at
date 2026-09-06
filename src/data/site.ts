c98e0a114c8f9e18241678c6315bdeccd7144415
Christoph Weiler <mail@chrisweiler.at>
2026-09-06 08:12:36 +0200

    Update phone number

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,8 @@
 export const site = {
 	name: 'Christoph Weiler',
 	email: 'mail@chrisweiler.at',
-	phone: '+43 660 148 2481',
-	phoneHref: 'tel:+436601482481',
+	phone: '+43 681 1057 7430',
+	phoneHref: 'tel:+4368110577430',
 	address: ['Sonnberg 57', '4261 Rainbach im Mühlkreis', 'Österreich'],
 	linkedin: 'https://www.linkedin.com/in/christoph-weiler-b768ba130/',
 	github: 'https://github.com/Lacuno',
